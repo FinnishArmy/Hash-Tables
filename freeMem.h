abc5ca717950846c2d9f70384746501b778773e7
Ronny <ronnyvaltonen@gmail.com>
2021-09-05 20:59:49 -0700

    Added files and moved things arround
    Change my way of getting the hash-code using the given crc64 file from
    the writeup.

---
+++ freeMem.h
@@ -0,0 +1,4 @@
+// This function simply resets the node to NULL, freeing
+// ... memory as it walks through the table. Finally,
+// ... it frees the nodes of the hash table.
+void freetable(hashtable *h);
